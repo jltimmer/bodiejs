d7a39b276a9291bfc010f627a39ba8ea0c2faf0d
Emma McCamey <mccameyec@mymail.vcu.edu>
2017-06-28 14:08:20 -0400

    made dialogue more understandable

--- bodie.js
+++ bodie.js
@@ -531,32 +531,35 @@ function talk(agent1, agent2) {
           knowledge.You++;
         }
         else if (knowledge.You == 1) {
-          text = "Congratulations you can now accuse William Hang of arson.</br></br>";
+          text = "Congratulations you have enough evidence to accuse William Hang of arson.</br></br>";
           if(knowledge.Hat == 1){
-            text += "Congratulations you can now accuse Shotgun Johnny of arson.</br></br>";
+            text += "Congratulations you have enough evidence to accuse Shotgun Johnny of arson.</br></br>";
           }
           if((knowledge.Letter == 1) && (knowledge.Insurance == 1)){
-            text += "Congratulations you can now accuse Shotgun Johnny and the Perrys of arson.</br></br>"
+            text += "Congratulations you have enough evidence to accuse Shotgun Johnny and the Perrys of arson.</br></br>"
           }
         }
       }
       else if (knowledge.Firehouse == 1) {
-        text = "</br > Sorry kid, the firehouse is gonna stay locked up " +
-          "tight unless you have a good reason to search it," +
-          " at the firefighters request.";
+        text = "You ask the Sheriff to unlock the firehouse.</br></br > " +
+               "The Sheriff responds, <q>" +
+               "Sorry kid, the firehouse is gonna stay locked up " +
+               "tight unless you have a good reason to search it, " +
+               "at the firefighters request.</q>";
       }
     }
     else if (agent2 == "Pat Reddy") {
       if (knowledge.PatReddy == 0) {
-        text = "<q>The only other thing I can talk about is the Perrys. They're in mourning " +
-          "for their lost property, I suspect.</q></br ></br > Before you can get " +
-          "in a word, he prattles on, </br ></br ><q>Yes, I represent them and " +
-          "their businesses. I am here to... look for any sign of " +
-          "foul play, and to reclaim any found valuables for the Perry " +
-          "estate. A kind and noble pair they are. If you seek them, " +
-          "check their home on Main Street, but I warn you, they're " +
-          "likely to be in a sad state indeed.</q></br ></br >" +
-          "<b>You now have access to the Perry and Reddy House</b>";
+        text = "You ask Pat Reddy to give you some information about the Perrys.</br></br>" +
+               "<q>They're in mourning " +
+               "for their lost property, I suspect.</q></br ></br > Before you can get " +
+               "in a word, he prattles on, </br ></br ><q>Yes, I represent them and " +
+               "their businesses. I am here to... look for any sign of " +
+               "foul play, and to reclaim any found valuables for the Perry " +
+               "estate. A kind and noble pair they are. If you seek them, " +
+               "check their home on Main Street, but I warn you, they're " +
+               "likely to be in a sad state indeed.</q></br ></br >" +
+               "<b>You now have access to the Perry and Reddy House</b>";
         locations.push("Perry House");
         locations.push("Reddy House");
         knowledge.PatReddy++;
@@ -566,60 +569,60 @@ function talk(agent1, agent2) {
     else if (agent2 == "Mr Perry") {
       if (knowledge.MrPerry == 0) {
         text = "The man with the eyepatch stands straight and tall over " +
-          "a squared off patch of land. He seems to be about sixty, " +
-          "with a strong physique and confident demeanor. He stares " +
-          "silently at the square. As you approach him, he looks you " +
-          "in the eyes and says, <q>This is the spot I've picked for " +
-          "my grave. You may find that odd, but somebody will have " +
-          "to do it. I've decided to take the task into my own hands.</q> " +
-          "</br></br><q>My name is James Perry. Mono County Supervisor. And you're " +
-          "the deputy who's been looking into the fire.</q>";
+               "a squared off patch of land. He seems to be about sixty, " +
+               "with a strong physique and confident demeanor. He stares " +
+               "silently at the square. As you approach him, he looks you " +
+               "in the eyes and says, <q>This is the spot I've picked for " +
+               "my grave. You may find that odd, but somebody will have " +
+               "to do it. I've decided to take the task into my own hands.</q> " +
+               "</br></br><q>My name is James Perry. Mono County Supervisor. And you're " +
+               "the deputy who's been looking into the fire.</q>";
         knowledge.MrPerry++;
       }
       else if ((knowledge.MrPerry == 1) && (knowledge.Letter == 1)) {
         text = "You show him the letter.</br></br>He smiles when you show him the " +
-          "letter he sent to Pat Reddy. </br></br><q>Well, you're " +
-          "quite the investigator, aren't you? Unfortunately, " +
-          "that's not going to hold up in court. It's speculative, illegally " +
-          "obtained, and with Reddy on my side no court will indict me on such " +
-          "evidence. Besides. This is a place of constant change. Fires occur " +
-          "naturally, after all - they destroy the dead wood.</q></br></br> He turns away " +
-          "from you.";
+               "letter he sent to Pat Reddy. </br></br><q>Well, you're " +
+               "quite the investigator, aren't you? Unfortunately, " +
+               "that's not going to hold up in court. It's speculative, illegally " +
+               "obtained, and with Reddy on my side no court will indict me on such " +
+               "evidence. Besides. This is a place of constant change. Fires occur " +
+               "naturally, after all - they destroy the dead wood.</q></br></br> He turns away " +
+               "from you.";
       }
     }
     else if (agent2 == "Shotgun Johnny") {
       if (knowledge.ShotgunJohnny == 0) {
         text = "The short man strikes a comical figure, with a bulldog face " +
-          "and a round black hat, similar to a sun hat.</br>You begin to " +
-          "walk towards the man, but as you do, he saunters towards you " +
-          "instead, breaking off his conversation. The tall man turns " +
-          "back to the patch of land, brooding.</br>In a thick cornish " +
-          "accent, he hollers, <q>Hello! I see you eyeing me. Well, I " +
-          "decided I'd come over here, eye you instead. The prop's to " +
-          "see how you like it.</q> He gets right up in your face. <q>They " +
-          "call me Shotgun Johnny. Wanna guess why?</q> Menace flickers " +
-          "in his eyes.</br></br>A tense moment passes, and he suddenly breaks out " +
-          "in laughter.</br></br><q>Nah, I'm playin' with ya. What is it you want to say?</q>" +
-          "There's still a bit of edge in his voice.";
-        knowledge.ShotgunJohnny++;
+               "and a round black hat, similar to a sun hat.</br>You begin to " +
+               "walk towards the man, but as you do, he saunters towards you " +
+               "instead, breaking off his conversation. The tall man turns " +
+               "back to the patch of land, brooding.</br>In a thick cornish " +
+               "accent, he hollers, <q>Hello! I see you eyeing me. Well, I " +
+               "decided I'd come over here, eye you instead. The prop's to " +
+               "see how you like it.</q> He gets right up in your face. <q>They " +
+               "call me Shotgun Johnny. Wanna guess why?</q> Menace flickers " +
+               "in his eyes.</br></br>A tense moment passes, and he suddenly breaks out " +
+               "in laughter.</br></br><q>Nah, I'm playin' with ya. What is it you want to say?</q>" +
+               "There's still a bit of edge in his voice.";
+               knowledge.ShotgunJohnny++;
       }
       else if ((knowledge.Hat == 1) && (knowledge.ShotgunJohnny == 1)) {
         text = "You show him the hat you found in the firehouse.</br></br><q>Yeah, " +
-          "that's me hat...</q></br></br>Johnny blinks.</br></br><q>Where'd you find " +
-          "that? I have another but that's my favorite. I got a prop " +
-          "for ya - give it back? Please?</q></br></br>He realizes that he's " +
-          "been made. <q></br></br>Damn. Alright, fine. I was there that night, " +
-          "but you can't pin the arson on me. There's no way I could " +
-          "have set the fire and sabotaged the water main at once. Now, " +
-          "can I at least have my hat back?</q></br></br>You refuse to give him " +
-          "the evidence.";
+               "that's me hat...</q></br></br>Johnny blinks.</br></br><q>Where'd you find " +
+               "that? I have another but that's my favorite. I got a prop " +
+               "for ya - give it back? Please?</q></br></br>He realizes that he's " +
+               "been made. <q></br></br>Damn. Alright, fine. I was there that night, " +
+               "but you can't pin the arson on me. There's no way I could " +
+               "have set the fire and sabotaged the water main at once. Now, " +
+               "can I at least have my hat back?</q></br></br>You refuse to give him " +
+               "the evidence.";
       }
     }
     else if ((agent2 == "Mrs Perry") && (knowledge.Insurance == 1)) {
       text = "You pull out the document that Cain gave you and hand it to her. " +
-        "Mrs. Perry is unimpressed.</br></br><q>Well? Why show me what I already " +
-        "know? JS Cain likely told you I was up to no good, eh? He's " +
-        "hated us for years.</q>";
+             "Mrs. Perry is unimpressed.</br></br><q>Well? Why show me what I already " +
+             "know? JS Cain likely told you I was up to no good, eh? He's " +
+             "hated us for years.</q>";
     }
 
     return text;
@@ -653,7 +656,8 @@ function give(agent1, agent2, thing) {
       }
     }
     if (graveYardAccess == 0) {
-      text = "No thank you, this " + thing + " is not mine.";
+      text = "You try to hand the " + thing + " to " + agent2 + ".</br></br>" +
+             agent2 + " responds, <q>No thank you, this " + thing + " is not mine.</q>";
     }
     else {
       text = agent1 + " give " + thing + " to " + agent2;
